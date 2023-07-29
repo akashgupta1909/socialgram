5bfbca0517dc176c6b6a3a99532207d88b09daba
Akash Gupta <akash.gupta.anup@gmail.com>
2023-07-29 23:01:35 +0530

    Changed metaData export

--- app/layout.js
+++ app/layout.js
@@ -7,15 +7,16 @@ import { ConfigureStore } from "./redux/configureStore";
 const store = ConfigureStore();
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Social Gram",
-  description: "Small social media app built with Next.js",
-};
+// export const metadata = {
+//   title: "Social Gram",
+//   description: "Small social media app built with Next.js",
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <title>Social Gram - Small social media app built with Next.js</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0"
